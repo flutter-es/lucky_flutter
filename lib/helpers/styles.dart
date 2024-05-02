eea63546336259c317b99d7a3ced99bf247e7c8e
Anya <arguellesdev@gmail.com>
2024-05-02 13:04:25 -0700

    adding redbox to the stack for LuckyMarker widget

--- lib/helpers/styles.dart
+++ lib/helpers/styles.dart
@@ -23,3 +23,31 @@ class LuckyContainer extends StatelessWidget {
     );
   }
 }
+
+class LuckySelectors extends StatelessWidget {
+  const LuckySelectors({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.arrow_right,
+          size: 80,
+          color: LuckyFlutterColors.markerBox,
+        ),
+        SizedBox(
+          width: 400,
+          height: 150,
+        ),
+        Icon(
+          Icons.arrow_left,
+          size: 80,
+          color: LuckyFlutterColors.markerBox,
+        ),
+      ],
+    );
+  }
+}
